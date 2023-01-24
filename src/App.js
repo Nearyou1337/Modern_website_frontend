c2ab8bcc6c2f8e44265cce82d05a24c5cddbd4fd
Nikita <nikitaloktev71@gmail.com>
2023-01-24 18:45:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {
+  Blog,
+  Features,
+  Footer,
+  Possibility,
+  Header,
+  WhatGPT,
+} from "./containers";
+import { Cta, Brand, Navbar } from "./components";
+import "./app.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from "./Article/article";
+export { default as Brand } from "./Brand/brand";
+export { default as Cta } from "./Cta/cta";
+export { default as Feature } from "./Feature/feature";
+export { default as Navbar } from "./Navbar/navbar";
